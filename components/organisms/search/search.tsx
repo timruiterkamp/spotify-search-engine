981dd3d9360f454a9125ebc2e374d399235c02e3
Tim <timruiterkamp@gmail.com>
2022-07-26 18:42:13 +0200

    fix: import

--- components/organisms/search/search.tsx
+++ components/organisms/search/search.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect } from "react";
-import SearchBar from "../../molecules/searchBar/SearchBar";
 import { gql, useLazyQuery, useQuery } from "@apollo/client";
 import client from "../../graphql/apollo-client";
 import { useSearchContext } from "../../context/SearchContext";
+import SearchBar from "../../molecules/searchBar/SearchBar";
 
 const Search: React.FC = () => {
   const { searchSpotify } = useSearchContext();
